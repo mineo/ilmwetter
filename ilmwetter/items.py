8b9305575edf29af847dfb74f2d1423ada152fea
Wieland Hoffmann <themineo@gmail.com>
2015-04-29 20:10:25 +0200

    sort-lines

--- ilmwetter/items.py
+++ ilmwetter/items.py
@@ -3,12 +3,12 @@ import scrapy
 
 
 class IlmwetterItem(scrapy.Item):
-    scraping_time = scrapy.Field()
-    temperature = scrapy.Field()
-    humidity = scrapy.Field()
     dew_point = scrapy.Field()
+    humidity = scrapy.Field()
+    precipitation_amount = scrapy.Field()
     pressure = scrapy.Field()
+    scraping_time = scrapy.Field()
+    solar_radiation = scrapy.Field()
+    temperature = scrapy.Field()
     wind_direction = scrapy.Field()
     wind_speed = scrapy.Field()
-    solar_radiation = scrapy.Field()
-    precipitation_amount = scrapy.Field()
