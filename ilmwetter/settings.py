f264bec40e56a4ef37ac1ac1f3d32551eb352a3d
Wieland Hoffmann <themineo@gmail.com>
2015-04-29 16:08:30 +0200

    save output to a csv file

--- ilmwetter/settings.py
+++ ilmwetter/settings.py
@@ -9,3 +9,6 @@ DEFAULT_ITEM_CLASS = 'ilmwetter.items.IlmwetterItem'
 ITEM_PIPELINES = {
     'ilmwetter.pipelines.IlmwetterPipeline': 100
 }
+
+FEED_URI = 'file:///tmp/scrapyfeed.csv'
+FEED_FORMAT = 'csv'
